b815297cfe8da9a2946eddef9fa01a4d598aa5cb
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-18 22:24:05 -0400

    register a rover on the matrix

--- spec/PlateauSpec.js
+++ spec/PlateauSpec.js
@@ -5,21 +5,26 @@ describe("Plateau", function() {
   var rover;
 
   beforeEach(function() {
-    // ...
+    plateau = new Plateau(5, 5).generate();
   });
 
   it("should generate a 2D array with 5 columns and 5 rows", function() {
-    plateau = new Plateau(5, 5).generate();
     expect(plateau.matrix[4][4]).toEqual(0);
     expect(plateau.matrix[5]).toBeUndefined();
   });
 
-  it("can add a Rover to a list of activeRovers", function() {
-    plateau = new Plateau(3, 3).generate();
+  it("should add a Rover to a list of activeRovers", function() {
     rover = new Rover();
     plateau.addRover(rover)
     plateau.addRover(rover)
     expect(plateau.activeRovers).toEqual([rover, rover]);
   })
 
+  it("should register a rover on the matrix according to it's coordinates", function() {
+    rover = new Rover();
+    rover.coordinates = {x: 1, y: 2};
+    rover.heading = "N";
+    plateau.registerRover(rover);
+    expect(plateau.matrix[2][1]).toBe(rover)
+  });
 });
