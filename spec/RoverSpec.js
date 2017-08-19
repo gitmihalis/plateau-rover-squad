547c764b52fd11c4e6ca76b51b7f5663c3088e31
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-18 21:28:02 -0400

    Rover rotates by 90 deg given L or R

---
+++ spec/RoverSpec.js
@@ -0,0 +1,34 @@
+describe("Rover", function() {
+  var Rover = require('../lib/Rover');
+  var rover;
+
+  beforeEach(function() {
+    // ...
+  });
+
+  it("should initialize an instance of Rover", function() {
+    rover = new Rover;
+    expect(rover).toEqual(jasmine.any(Rover));
+  });
+
+  it("should rotate to 0deg given LLRRRL", function() {
+    rover = new Rover;
+    rover.rotate('L').rotate('L').rotate('R');
+    rover.rotate('R').rotate('R').rotate('L');
+    expect(rover.heading).toBe(0)
+  })
+
+  it("should rotate to 90deg given RRRRR", function() {
+    rover = new Rover;
+    rover.rotate('R').rotate('R').rotate('R').rotate('R').rotate('R');
+    expect(rover.heading).toBe(0)
+  })
+
+  it("should rotate to 90deg given LLRRRLR", function() {
+    rover = new Rover;
+    rover.rotate('L').rotate('L').rotate('R');
+    rover.rotate('R').rotate('R').rotate('L').rotate('R');
+    expect(rover.heading).toBe(0)
+  })
+
+});
