bb47928e00d9ca66f62769c5991a3efe989ae840
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-19 23:58:46 -0400

    refactor tests

--- spec/RoverSpec.js
+++ spec/RoverSpec.js
@@ -3,32 +3,35 @@ describe("Rover", function() {
   var rover;
 
   beforeEach(function() {
-    // ...
+    rover = new Rover
   });
 
   it("should initialize an instance of Rover", function() {
-    rover = new Rover;
     expect(rover).toEqual(jasmine.any(Rover));
   });
 
   it("should rotate to 0deg given LLRRRL", function() {
-    rover = new Rover;
     rover.rotate('L').rotate('L').rotate('R');
     rover.rotate('R').rotate('R').rotate('L');
     expect(rover.heading).toBe(0)
   })
 
   it("should rotate to 90deg given RRRRR", function() {
-    rover = new Rover;
     rover.rotate('R').rotate('R').rotate('R').rotate('R').rotate('R');
     expect(rover.heading).toBe(0)
   })
 
   it("should rotate to 90deg given LLRRRLR", function() {
-    rover = new Rover;
     rover.rotate('L').rotate('L').rotate('R');
     rover.rotate('R').rotate('R').rotate('L').rotate('R');
     expect(rover.heading).toBe(0)
+  })
+
+  it("should move in the direction of its heading", function() {
+    rover.heading = 'S';
+    rover.coordinates = {x: 2, y:2};
+    rover.move();
+    expect(rover.coordinates).toBe({x:2, y:1});
   })
 
   
