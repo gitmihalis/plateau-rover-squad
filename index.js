be1d2629d8e4b4a16eaaf655182a9cfaa9660c46
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-20 20:55:11 -0400

    parse and generate plateau from text file

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
+////////////////////////////////////////////////////////////////////
+// Rover Dispatcher ... 
+// Receives instructions from a text file and moves rovers through a
+// matrix , representing a plateau that is being scanned for lifeforms!
+//
+// TODO
+// [ ] initiate a rover with an instruction
+// [ ] Create a helper to alternate between generating a rover and moving that rover
+
+// new Plateau takes ( rows, columns ) as args 
 const Plateau = require('./lib/Plateau')
+
+// new Rover takes (x, y) as args
 const Rover = require('./lib/Rover')
 
-// Supply input data to your rovers via a text file specified in the command line.
+// Establish instructions from text file
 const fs = require('fs')
 const readline = require('readline')
 const file = process.argv[2]
 
+// Create a readable stream of our input file
 const rl = readline.createInterface({
   input: fs.createReadStream(file)
 })
 
-// Parse the list of commands
+
+////////////////////////////////////////////////////////
+// Parse the sets of commands from our readable Stream
+//
 const parseInstructions = new Promise( (resolve, reject) => {
   let parsed = []
   rl.on('line', (line) => parsed.push(line))
@@ -22,9 +38,19 @@ const parseInstructions = new Promise( (resolve, reject) => {
   })
 })
 
-// Run the app after we get our instructions
-parseInstructions.then( (lines) => {
+
+////////////////////////////////////////////////////////////////////////////
+// Crete the plateau and dispatch the rovers AFTER we get our instructions
+//
+parseInstructions.then( (parsed) => { // I for instructions
+  const instructions = parsed
+  let plateauParams = instructions.splice(0,1)[0]
+    .map( param => parseInt(param))
+
+  // initialize the plateau and generate the matrix
+  const plateau = new Plateau(plateauParams[1], plateauParams[0]).generate()
   
-  console.log(lines)
+  console.log(plateau)
+
 })
 .catch( (error) => console.error(error))
